13252233e98e3ccb2bdc03d981fabfd8e141e3c4
Abhay Rai <abhayrai@Abhays-MacBook-Air.local>
2022-07-14 00:28:45 +0530

    modal overlay

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React,{useState } from 'react';
 import './App.css';
 import { Circles } from  'react-loader-spinner'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal} from 'react-bootstrap';
 
 
 function App() {
 
+
+  const [show, setShow] = useState(false);
+  const [showpass,setShowPass]=useState(false)
+
   const [item,setitem]=useState("")
+  const [pass,setpass]=useState("")
   const [disable, setDisable] = useState(false);
 const [loading,setLoading]=useState(false);
 const [active,setactive]=useState(false)
 
+
 const handleInput=(e)=>{
+   e.preventDefault();
     setitem(e.target.value);
+}
+const handlePassword=(e)=>{
+  setpass(e.target.value);
 }
 
 const handlereset=()=>{
@@ -20,12 +32,29 @@ const handlereset=()=>{
    setDisable(false);
 }
  const handleLoader=()=>{
+  if(item==="")
+  {
+    setShow(true)
+    setTimeout(()=>{
+      setShow(false)
+    },2000)
+  }
+  else if(pass==="")
+  {
+    setShowPass(true)
+    setTimeout(()=>{
+      setShowPass(false)
+    },3000)
+  }
+  else
+  {
     setLoading(true);
     setDisable(true)
     setTimeout(()=>{
       setLoading(false)
       setactive(true)
     },3000)
+  }
  }
 
 
@@ -34,10 +63,21 @@ const handlereset=()=>{
       {active===false?
       <>
        <input type="text" placeholder="Enter Name"  onChange={handleInput}/><br />
-       <input type="number" placeholder="Enter Password" /><br />
+       <input type="number" placeholder="Enter Password" onChange={handlePassword} /><br />
        <button disabled={disable}   onClick={handleLoader} style={{cursor:"pointer"}} type="button">{loading===false?"Login":<Circles color="#00BFFF" height={10} width={25}/>}</button>
        </>:<div onClick={()=>handlereset()} style={{cursor:"pointer"}}><h1>Welcome {item}</h1></div>}
+       <Modal show={show} >
+        <Modal.Header >
+        </Modal.Header>
+        <Modal.Body>Oooooppsss! Input cannot be Empty</Modal.Body>
+      </Modal>
+      <Modal show={showpass} >
+        <Modal.Header >
+        </Modal.Header>
+        <Modal.Body>Oooooppsss! Password cannot be Empty</Modal.Body>
+      </Modal>
     </div>
+
   );
 }
 
